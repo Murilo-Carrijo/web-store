ec2f75fc5b0f5d462c27dbc73b7b947dc5f6d663
murilo <murilocarrijoadm@gmail.com>
2025-01-21 19:47:32 -0300

    Foi criado uma requisição do tipo `get` para validar a conexão com o banco de dados

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5432;
+const port = process.env.PORT || 3000;
+const database = require("./infra/database");
 
-app.get("/", (req, res) => {
-  res.status(200).json({messge: "Hello World"});
+app.get("/", async (req, res) => {
+  const result = await database.query("SELECT NOW()");
+  console.log(result.rows[0]);
+  res.status(200).json(result.rows[0]);
 });
 
 app.listen(port, (err) => {
