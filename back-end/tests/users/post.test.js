90054354e7e28ba1ab118cef75423955c53b1fc5
murilo <murilocarrijoadm@gmail.com>
2025-01-22 12:53:53 -0300

    Foi adicionado verificação de usuário ja existe e inpust incorretos ao testes da rota post de usuários

--- back-end/tests/users/post.test.js
+++ back-end/tests/users/post.test.js
@@ -1,14 +1,80 @@
-test("POST to /status returns 200", async () => {
-  const response = await fetch("http://localhost:3000/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: "teste@teste.com",
-      password: "password123",
-    }),
+const database = require("../setup");
+
+describe("POST /users", () => {
+  beforeAll(async () => {
+    await database.query('DROP TABLE IF EXISTS users;');
+    await database.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(50) NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updateAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
   });
 
-  expect(response.status).toBe(201);
+  afterAll(async () => {
+    await database.query('DROP TABLE IF EXISTS users;');
+  });
+
+  test("POST to /users returns 201", async () => {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "teste@teste.com",
+        password: "password123",
+      }),
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  test("POST to /users returns 400 se o usuário já existir", async () => {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "teste@teste.com",
+        password: "password123",
+      }),
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  test("POST to /users returns 400", async () => {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "teste@teste.com",
+        password: '',
+      }),
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  test("POST to /users returns 400", async () => {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "",
+        password: 'password123',
+      }),
+    });
+
+    expect(response.status).toBe(400);
+  });
 });
