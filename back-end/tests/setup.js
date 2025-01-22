a85a5c4ffab89e182df06df8d38cc622aa3510fe
murilo <murilocarrijoadm@gmail.com>
2025-01-22 12:52:01 -0300

    Foi criado um novo setup para testes

---
+++ back-end/tests/setup.js
@@ -0,0 +1,29 @@
+const { Client } = require('pg');
+require('dotenv').config(".env.test");
+
+const query = async (queryObject) => {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+};
+
+
+module.exports = {
+  query,
+}
+
+
