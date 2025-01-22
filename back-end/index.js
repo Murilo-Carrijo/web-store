c71aaabea475fc8970e75328bfbfd8af5bf42131
murilo <murilocarrijoadm@gmail.com>
2025-01-22 05:43:21 -0300

    A rota `/` foi substituida pela rota `/status` e foi refatorda para atender os testes

--- back-end/index.js
+++ back-end/index.js
@@ -5,10 +5,25 @@ const app = express();
 const port = process.env.PORT || 3000;
 const database = require("./infra/database");
 
-app.get("/", async (req, res) => {
-  const result = await database.query("SELECT NOW()");
-  console.log(result.rows[0]);
-  res.status(200).json(result.rows[0]);
+app.get("/status", async (req, res) => {
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionResult = dbVersion.rows[0].server_version;
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsResult = parseInt(dbMaxConnections.rows[0].max_connections);
+  const dbOpenConnections = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const dbOpenConnectionsResult = parseInt(dbOpenConnections.rows[0].count);
+
+  result = {
+    update_at: new Date().toISOString(),
+    server_version: dbVersionResult,
+    max_connections: dbMaxConnectionsResult,
+    open_connections: dbOpenConnectionsResult,
+  }
+
+  return res.status(200).json(result);
 });
 
 app.listen(port, (err) => {
