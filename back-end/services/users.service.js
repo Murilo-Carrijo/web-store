ceea80238ffc8697cad6155399de315078af4f68
murilo <murilocarrijoadm@gmail.com>
2025-01-23 20:24:31 -0300

    A função `createUser` foi refatorada para passar nos testes considerando o nome

--- back-end/services/users.service.js
+++ back-end/services/users.service.js
@@ -3,14 +3,17 @@ const crypto = require('crypto');
 const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
 const createUser = async ({ email, password, name }) => {
+  const trimNmae = name.trim();
   try {
-    if (!email || email.length === 0 || !password || password.length === 0) {
-      throw new Error("Missing parameters");
-    }
+    if (!email || email.length === 0) throw new Error("Missing parameters");
+    if (!password || password.length === 0) throw new Error(`Missing parameters${password}`);
+    if (!trimNmae || trimNmae.length === 0) throw new Error("Missing parameters");
+
     const checkEmail = emailRegex.test(email);
     if (!checkEmail) throw new Error("This email is invalid");
     const user = await userModel.getByEmail(email);
     if (password.length < 6) throw new Error("The password must be at least 6 characters");
+    if (trimNmae.length < 3) throw new Error("The name must be at least 3 characters");
 
     if (user) throw new Error("User already exists");
 
