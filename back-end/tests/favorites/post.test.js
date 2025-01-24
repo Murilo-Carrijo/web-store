db02b54e036ba04d40f887dc9127762f419970a4
murilo <murilocarrijoadm@gmail.com>
2025-01-23 21:36:25 -0300

    Foram adicionados novos testes para cobrir a falta de elementos e caso a lista esteja cheia

--- back-end/tests/favorites/post.test.js
+++ back-end/tests/favorites/post.test.js
@@ -14,9 +14,7 @@ describe("POST /favorites", () => {
         category: "papelaria",
         description: "descrição",
         image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
-        userId: 1,
-        createdAt: new Date().toISOString(),
-        updateAt: new Date().toISOString()
+        userId: 1
       }),
     });
 
@@ -38,14 +36,144 @@ describe("POST /favorites", () => {
         category: "papelaria",
         description: "descrição",
         image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
-        userId: 1,
-        createdAt: new Date().toISOString(),
-        updateAt: new Date().toISOString()
+        userId: 1
       }),
     });
 
     expect(response.status).toBe(400);
     const result = JSON.parse(await response.text());
     expect(result.message).toBe("The externalId is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 price is required", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 1,
+        title: "title",
+        price: "",
+        category: "papelaria",
+        description: "descrição",
+        image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
+        userId: 1
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The price is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 category is required", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 1,
+        title: "title",
+        price: "50.0",
+        category: "",
+        description: "descrição",
+        image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
+        userId: 1
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The category is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 description is required", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 1,
+        title: "title",
+        price: "50.0",
+        category: "papelaria",
+        description: "",
+        image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
+        userId: 1
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The description is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 image is required", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 1,
+        title: "title",
+        price: "50.0",
+        category: "papelaria",
+        description: "descrição",
+        image: "",
+        userId: 1
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The image is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 userId is required", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 1,
+        title: "title",
+        price: "50.0",
+        category: "papelaria",
+        description: "descrição",
+        image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
+        userId: null
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The userId is missing parameters");
+  });
+
+  test("POST to /favorites returns 400 if the list alrady have 5 items", async () => {
+    const response = await fetch("http://localhost:3000/favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: 6,
+        title: "title",
+        price: "50.0",
+        category: "papelaria",
+        description: "descrição",
+        image: "https://img.freepik.com/fotos-gratis/caderno_74190-4422.jpg",
+        userId: 1
+      }),
+    });
+
+    expect(response.status).toBe(400);
+    const result = JSON.parse(await response.text());
+    expect(result.message).toBe("The userId is missing parameters");
   });
 });
