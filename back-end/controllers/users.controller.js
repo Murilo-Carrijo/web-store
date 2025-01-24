2b8bee10247fc045c2eb37c67c25b75ec037119e
murilo <murilocarrijoadm@gmail.com>
2025-01-23 23:01:38 -0300

    Console logs removidos

--- back-end/controllers/users.controller.js
+++ back-end/controllers/users.controller.js
@@ -3,8 +3,6 @@ const userService = require("../services/users.service");
 const createUser = async (req, res) => {
   try {
     await userService.createUser(req.body);
-    console.log('req.body', req.body);
-
     return res.status(201).json({ message: "User created"});
   } catch (error) {
     return res.status(400).json({ message: error.message });
@@ -14,9 +12,6 @@ const createUser = async (req, res) => {
 const login = async (req, res) => {
   try {
     const user = await userService.login(req.body);
-    console.log('req.body', req.body);
-    console.log('user', user);
-
     return res.status(200).json(user);
   } catch (error) {
     return res.status(400).json({ message: error.message });
