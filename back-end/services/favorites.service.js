94342b8174dc0bbe71acb91ac32c3174e70ee5eb
murilo <murilocarrijoadm@gmail.com>
2025-01-23 23:01:10 -0300

    Validação de produto em duplicidade implementada

--- back-end/services/favorites.service.js
+++ back-end/services/favorites.service.js
@@ -2,11 +2,22 @@ const favoritesModel = require('../models/favorites.model');
 
 const createFavorites = async (values) => {
   Object.entries(values).forEach(([key, value]) => {
-    console.log(`${key}: ${value}`);
     if (!value || value.length === 0) {
       throw new Error(`The ${key} is missing parameters`);
     }
   });
+
+  const favotiteList = await favoritesModel.getFavoritesByUserId(values.userId);
+
+  favotiteList.forEach(favorite => {
+    if(favorite.externalId === values.externalId) {
+      throw new Error('The element already exist');
+    }
+  });
+
+  if(favotiteList.length >= 5) {
+    throw new Error('The user already has 5 favorites');
+  }
 
   return await favoritesModel.createFavorites(values);
 };
