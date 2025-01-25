fc3343c16e61f28bdfff5317c8d1e1d540c25087
murilo <murilocarrijoadm@gmail.com>
2025-01-25 16:40:24 -0300

    Ajustando o css e o alinhamento dos elementos

--- front-end/src/components/card.jsx
+++ front-end/src/components/card.jsx
@@ -1,32 +1,42 @@
 const Card = ({ product }) => {
   return (
     <div key={product.id} className="card" style={{ display: 'inline-block', margin: '10px', width: '300px' }}>
-        <img
-          src={product.image}
-          alt={product.title}
-          style={{
-            height: '300px',
-            width: '100%',
-            objectFit: 'contain'
-          }}
-        />
-        <div className="card-body">
-          <div>
-          <h6 className="card-title" style={{ fontWeight: "600", height: "33.33%" }}>
+      <img
+        src={product.image}
+        alt={product.title}
+        style={{
+          height: '300px',
+          width: '100%',
+          objectFit: 'contain'
+        }}
+      />
+      <div className="card-body">
+        <div style={{ textAlign: 'start', height: '33.33%' }}>
+          <h6
+            className="card-title"
+            style={{
+              fontWeight: "600",
+              fontSize: '16px',
+              marginBottom: '8px',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+            title={product.title}>
             {product.title}
           </h6>
-            <p className="card-text">R$ {product.price}</p>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <a className="btn btn-primary" style={{ color: 'white', height: "33.33%" }}>
-            Mais detalhes
-          </a>
-          <button style={{ all: 'unset' }}>
-            <i className="fa-solid fa-star" style={{color:  "#fbb913"}}></i>
-          </button>
-          </div>
+          <p className="card-text" style={{marginBottom: '8px',}}>R$ {product.price}</p>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <a className="btn btn-primary" style={{ color: 'white', height: "33.33%" }}>
+          Mais detalhes
+        </a>
+        <button style={{ all: 'unset' }}>
+          <i className="fa-solid fa-star" style={{color:  "#fbb913"}}></i>
+        </button>
         </div>
       </div>
+    </div>
   )
 }
 
