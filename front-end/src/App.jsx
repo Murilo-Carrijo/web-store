6467eeeba98c483d33cfec31c5fa70a5fe4f5a15
murilo <murilocarrijoadm@gmail.com>
2025-01-25 16:40:45 -0300

    a tela de produtos foi criada

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Favorites from './pages/favorites';
+import Products from './pages/products';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/favorites" element={<Favorites />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   )
 }
