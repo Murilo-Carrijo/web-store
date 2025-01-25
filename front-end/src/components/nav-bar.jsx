2557e564feea292601e2141d3afc2197ee638f8b
murilo <murilocarrijoadm@gmail.com>
2025-01-25 17:40:58 -0300

    O nav bar foi refatorado para que tanto o link da home quanto o dos favoritos sejam renderizados na pagina do produto

--- front-end/src/components/nav-bar.jsx
+++ front-end/src/components/nav-bar.jsx
@@ -3,14 +3,27 @@ import { useState } from 'react';
 
 const NavBar = () => {
   const [currentUrl, _setCurrentUrl] = useState(window.location.pathname);
+
+  let navLink;
+  switch (currentUrl) {
+    case '/':
+      navLink = <NavLink className='navbar-brand' to="/favorites">Favoritos</NavLink>;
+      break;
+    case '/favorites':
+      navLink = <NavLink className='navbar-brand' to="/">Home</NavLink>;
+      break;
+    default:
+      navLink = (
+        <div style={{width: '50%', display: "flex", justifyContent: "end", alignItems: 'center'}}>
+          <NavLink className='navbar-brand' to="/">Home</NavLink>
+          <NavLink className='navbar-brand' to="/favorites">Favoritos</NavLink>
+        </div>
+      );
+  }
   return (
     <nav className='navbar navbar-light'>
       <div>Web Store</div>
-      {currentUrl === '/' ? (
-        <NavLink className='navbar-brand' to="/favorites">Favoritos</NavLink>
-      ) : (
-        <NavLink className='navbar-brand' to="/">Home</NavLink>
-      )}
+      { navLink }
     </nav>
   )
 }
