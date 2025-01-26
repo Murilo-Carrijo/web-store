73a50dd068b4f404173ff543ec5744a5abc24107
murilo <murilocarrijoadm@gmail.com>
2025-01-25 23:52:38 -0300

    O fetch para a rota de cadastro foi criado

--- front-end/src/services/user_services.js
+++ front-end/src/services/user_services.js
@@ -14,3 +14,20 @@ export const authenticate = async (email, password) => {
   const token = await response.json();
   return token;
 };
+
+export const register = async (name, email, password) => {
+  const response = await fetch('http://localhost:3000/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, email, password })
+  });
+
+  if (!response.ok) {
+    throw new Error('Error registering user');
+  }
+
+  const token = await response.json();
+  return token;
+}
