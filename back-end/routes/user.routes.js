f4e3c413262067cab85505ec7bcb7bf40d32a150
murilo <murilocarrijoadm@gmail.com>
2025-01-28 11:19:02 -0300

    Corrigindo erro do middleware

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const UserController = require("../controllers/users.controller");
-const { authenticateToken } = require("../middleware/authorizarion");
+const authenticateToken = require("../middleware/authorizarion");
 
 const router = express.Router();
 
 const user = new UserController();
 router.post("/user/create", (req, res) => user.createUser(req, res));
 router.post("/login", (req, res) =>  user.login(req, res));
-router.delete("/user/delete", (req, res, next) => {
-  authenticateToken(req, res, next);
-  user.deleteUserById(req, res);
-});
+router.delete("/user/delete", authenticateToken, (req, res) => user.deleteUserById(req, res));
 
 module.exports = router;
