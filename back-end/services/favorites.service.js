fb7308d1e5bbb974a16c93fa020962c9745205cb
murilo <murilocarrijoadm@gmail.com>
2025-01-28 11:19:41 -0300

    Favorites services alterada para utlizar classes

--- back-end/services/favorites.service.js
+++ back-end/services/favorites.service.js
@@ -1,54 +1,64 @@
-const favoritesModel = require('../models/favorites.model');
+const FavoritesModel = require('../models/favorites.model');
 
-const createFavorites = async (values, userId) => {
-  Object.entries(values).forEach(([key, value]) => {
-    if (!value || value.length === 0) {
-      throw new Error(`The ${key} is missing parameters`);
-    }
-  });
-
-  const favotiteList = await favoritesModel.getFavoritesByUserId(userId);
-
-  favotiteList.forEach(favorite => {
-    if(favorite.externalId === values.externalId) {
-      throw new Error('The element already exist');
-    }
-  });
-
-  if(favotiteList.length >= 5) {
-    throw new Error('The user already has 5 favorites');
+class FavoritesService {
+  constructor() {
+    this.modal = new FavoritesModel();
   }
 
-  return await favoritesModel.createFavorites({values, userId});
-};
+  createFavorites = async (values, userId) => {
+    Object.entries(values).forEach(([key, value]) => {
+      if (!value || value.length === 0) {
+        throw new Error(`The ${key} is missing parameters`);
+      }
+    });
 
-const getFavoritesByUserId = async (userId) => {
-  return await favoritesModel.getFavoritesByUserId(userId);
-};
+    const favotiteList = await this.modal.getFavoritesByUserId(userId);
 
-const deleteAllFavoritesByUserId = async (userId) => {
-  const favoriteList = await favoritesModel.getFavoritesByUserId(userId);
+    favotiteList.forEach(favorite => {
+      if(favorite.externalId === values.externalId) {
+        throw new Error('The element already exist');
+      }
+    });
 
-  if (favoriteList.length === 0) throw new Error('The user does not have favorites');
+    if(favotiteList.length >= 5) {
+      throw new Error('The user already has 5 favorites');
+    }
 
-  return await favoritesModel.deleteAllFavoritesByUserId(userId);
-};
+    return await this.modal.createFavorites({values, userId});
+  };
 
-const deleteByFavoriteId = async (userId, favoriteId) => {
-  const favoriteList = await favoritesModel.getFavoritesByUserId(userId);
+  getFavoritesByUserId = async (userId) => {
+    const favoriteList = await this.modal.getFavoritesByUserId(userId);
+    if (favoriteList.length === 0) throw new Error('The user does not have favorites');
+    return favoriteList;
+  };
 
-  if (favoriteList.length === 0) throw new Error('The user does not have favorites');
+  deleteAllFavoritesByUserId = async (userId) => {
+    const favoriteList = await this.modal.getFavoritesByUserId(userId);
 
-  const favorite = favoriteList.find(favorite => favorite.id === favoriteId);
+    if (favoriteList.length === 0) throw new Error('The user does not have favorites');
 
-  if (!favorite) throw new Error('The favorite does not exist');
+    return await this.modal.deleteAllFavoritesByUserId(userId);
+  };
 
-  return await favoritesModel.deleteByFavoriteId(userId, favoriteId);
-};
+  deleteByFavoriteId = async (userId, favoriteId) => {
+    const favoriteList = await this.getFavoritesByUserId(userId);
 
-module.exports = {
-  createFavorites,
-  getFavoritesByUserId,
-  deleteAllFavoritesByUserId,
-  deleteByFavoriteId
-};
+    if (favoriteList.length === 0) throw new Error('The user does not have favorites');
+
+    const favorite = favoriteList.find(favorite => favorite.id === favoriteId);
+
+    if (!favorite) throw new Error('The favorite does not exist');
+
+    await this.modal.deleteByFavoriteId(userId, favoriteId)
+
+    return "Favorite deleted";
+  };
+
+}
+
+
+
+
+
+module.exports = FavoritesService;
